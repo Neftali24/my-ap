fc8975c7d499d7d9f971807033a8959bc313f578
Neftali24 <yoneftali@gmail.com>
2023-02-08 19:39:52 -0600

    proyecto myapp

---
+++ src/main/java/com/nef/myapp/entity/TestEntity.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.nef.myapp.entity;
+
+/**
+ * @author yonef
+ *
+ */
+public class TestEntity {
+
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 * */
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+	
+}
